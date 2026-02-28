335cb92d7c62d1ebc70b2a8aab8c78187119cae6
ayumid2025 <bible9683@gmail.com>
2026-02-28 17:40:49 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,11 @@
+from flask import Flask, send_file
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # This sends the index.html file from the frontend folder
+    return send_file('../frontend/index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
